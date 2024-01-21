6f1e271b6f22ccc8eccde658cbb5e769a99df319
ShaikAbdul01 <shaikabdulahad2001@gmail.com>
2024-01-21 15:04:30 +0600

    2d array (1st)

---
+++ 2d_input_output.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int ar[row][col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            // printf("ar[%d]ar[%d] ", i, j);
+            scanf("%d", &ar[i][j]);
+        }
+        // printf("\n");
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            // printf("ar[%d]ar[%d] ", i, j);
+            printf("%d ", ar[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
